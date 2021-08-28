d5c2e8da91a661d9c4d14f7cce84203f64522758
fofonotholt <fofo.notholt@gmail.com>
2021-08-27 23:02:12 -0500

    Uploading challenge files

--- scraping.py
+++ scraping.py
@@ -6,26 +6,6 @@ import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def scrape_all():
-    # Initiate headless driver for deployment
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
-
-    news_title, news_paragraph = mars_news(browser)
-
-    # Run all scraping functions and store results in a dictionary
-    data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
-
-    # Stop webdriver and return data
-    browser.quit()
-    return data
-
 
 def mars_news(browser):
 
@@ -96,6 +76,49 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+# Challenge code ####
+def hemisphere(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemisphere_image_urls = []
+    for item in range(4):
+        hemisphere = {}
+        browser.find_by_tag("h3")[item].click()
+    
+        sample_element = browser.find_link_by_text("Sample").first
+        hemisphere["img_url"] = sample_element["href"]
+
+        hemisphere["title"] = browser.find_by_tag("h2").text
+
+        hemisphere_image_urls.append(hemisphere)
+
+        browser.back()
+
+    return hemisphere_image_urls
+
+
+####################
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "hemispheres" : hemisphere(browser),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 if __name__ == "__main__":
 
